996c5354dac26eefeedcd4974c770b2986f3657c
Palakurthi Lakshmi Srinivas <130201966+Lakshmisrinivaspalakurthi@users.noreply.github.com>
2026-06-17 14:06:07 +0530

    Update ranking.py

--- src/ranking.py
+++ src/ranking.py
@@ -1,13 +1,14 @@
 from sklearn.metrics.pairwise import cosine_similarity
 
 def calculate_score(
-    resume_embedding,
-    jd_embedding
-):
+        resume_embedding,
+        jd_embedding):
 
     score = cosine_similarity(
         [resume_embedding],
         [jd_embedding]
     )
 
-    return float(score[0][0]) * 100
+    return float(
+        score[0][0]
+    ) * 100
