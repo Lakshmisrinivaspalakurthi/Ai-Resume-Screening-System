a6232b9f9671888276e05c04f238e39ededbe724
Palakurthi Lakshmi Srinivas <130201966+Lakshmisrinivaspalakurthi@users.noreply.github.com>
2026-06-17 14:37:58 +0530

    Update ats.py

--- src/ats.py
+++ src/ats.py
@@ -1,43 +1,25 @@
-import re
-
-from src.skills import (
-    extract_skills
-)
+from src.skills import extract_skills
 
 def calculate_ats_score(
-        resume_text,
-        jd_text):
+    resume_text,
+    jd_text
+):
 
     resume_skills = set(
-        extract_skills(
-            resume_text
-        )
+        extract_skills(resume_text)
     )
 
     jd_skills = set(
-        extract_skills(
-            jd_text
-        )
+        extract_skills(jd_text)
     )
 
-    matched = (
-        resume_skills
-        &
-        jd_skills
-    )
+    matched = resume_skills & jd_skills
 
-    missing = (
-        jd_skills
-        -
-        resume_skills
-    )
+    missing = jd_skills - resume_skills
 
     if len(jd_skills) == 0:
-
         score = 0
-
     else:
-
         score = (
             len(matched)
             /
@@ -45,57 +27,7 @@ def calculate_ats_score(
         ) * 100
 
     return (
-        round(score, 2),
+        round(score,2),
         list(matched),
         list(missing)
     )
-
-def extract_experience(text):
-
-    pattern = (
-        r"(\\d+)\\+?\\s+years"
-    )
-
-    result = re.findall(
-        pattern,
-        text.lower()
-    )
-
-    if result:
-
-        return max(
-            [
-                int(x)
-                for x in result
-            ]
-        )
-
-    return 0
-
-def extract_education(text):
-
-    text = text.lower()
-
-    education = []
-
-    keywords = [
-
-        "b.tech",
-        "bachelor",
-
-        "m.tech",
-        "master",
-
-        "mba",
-        "phd"
-    ]
-
-    for key in keywords:
-
-        if key in text:
-
-            education.append(
-                key
-            )
-
-    return education
